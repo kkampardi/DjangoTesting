deaddd576dc4771892b236fd14baf5da32860f13
Katerina Kampardi <kkampardi@gmail.com>
2016-11-30 16:13:16 +0200

    Front page markup now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from selenium.webdriver.common.keys import Keys
 
 from lists.views import home_page
 
