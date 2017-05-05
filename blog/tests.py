9cf1a2367e0b8a4ca74d0e8dd4b35d028078ec45
pycat <kkampardi@gmail.com>
2017-05-05 13:26:46 +0300

    Test Model Meta class  #2

--- blog/tests.py
+++ blog/tests.py
@@ -8,3 +8,6 @@ class EntryModelTest(TestCase):
     def test_string_representation(self):
         entry = Entry(title="This is a test title")
         self.assertEqual(str(entry), entry.title)
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(Entry._meta.verbose_name_plural), "entries")
