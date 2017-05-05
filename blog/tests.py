9bff96e198e3d53c006d278f7dd6bdd78a6baeb8
pycat <kkampardi@gmail.com>
2017-05-05 13:14:11 +0300

    Test string representation #2
    - that the a blog's entry string representation is qual to its title
    - fix the string representation using __str__ special method

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Entry
+
+class EntryModelTest(TestCase):
+    """Ensure that the a blog's entry string representation is qual to its title"""
+
+    def test_string_representation(self):
+        entry = Entry(title="This is a test title")
+        self.assertEqual(str(entry), entry.title)
