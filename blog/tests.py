1092c41d9d4930f0512fac79b4d95836c70e5f3a
pycat <kkampardi@gmail.com>
2017-05-09 10:26:30 +0300

    Test the class based views #3

--- blog/tests.py
+++ blog/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 from .models import Entry
 
@@ -13,7 +14,29 @@ class EntryModelTest(TestCase):
         self.assertEqual(str(Entry._meta.verbose_name_plural), "entries")
 
 
-class ProjectTests(TestCase):
+class HomePageTest(TestCase):
+    """Test whether our blog entries show up on the homepage"""
+    def setUp(self):
+        self.user = get_user_model().objects.create(username="some_user")
+
     def test_homepage(self):
-        response = self.client.get('/blog')
+        response = self.client.get('/blog/list')
         self.assertEqual(response.status_code, 200)
+
+    def test_one_entry(self):
+        Entry.objects.create(title='1-title', body='1-body', author=self.user)
+        response = self.client.get('/blog/list')
+        self.assertContains(response, '1-title')
+        self.assertContains(response, '1-body')
+
+    def test_no_entries(self):
+        response = self.client.get('/blog')
+        self.assertContains(response, 'No blog entries yet')
+
+    def test_two_entries(self):
+        Entry.objects.create(title='1-title', body='1-body', author=self.user)
+        Entry.objects.create(title='2-title', body='2-body', author=self.user)
+        response = self.client.get('/')
+        self.assertContains(response, '1-title')
+        self.assertContains(response, '1-body')
+        self.assertContains(response, '2-title')
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
+from .models import  Entry
+
 
 class HomeView(TemplateView):
 
     template_name = 'index.html'
+    queryset = Entry.objects.order_by('-created_at')
+
